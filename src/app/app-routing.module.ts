526f65f358aac568a56ade2fb5d1965421224449
joselinegab <joselyngab.castro@gmail.com>
2023-01-07 18:51:23 -0500

    Navbar-Taller

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'precios', component:PreciosComponent},
+  {path:'protegida',component:ProtegidaComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
